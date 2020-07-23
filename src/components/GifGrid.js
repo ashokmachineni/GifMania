5d9911ec6208537e91a0b3d5d39ba7fad9833b69
ashokmachineni <ashok5679m@gmail.com>
2020-07-23 15:56:00 +0100

    removed unused css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -2,14 +2,14 @@ import React from "react";
 import GifGridItem from "./GifGridItem";
 import { getGifs } from "../helpers/getGifs";
 import { useFetchGifs } from "../hooks/useFetchGifs";
-import { CardGroup } from "react-bootstrap";
+
 function GifGrid({ category }) {
   const { data, loading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
-      {loading && <p>Loading...</p>}
+      {loading && <p className="animate__slideOutRight">Loading...</p>}
       <div className="card-grid">
         {data.map(imag => (
           <GifGridItem key={imag.id} {...imag} />
